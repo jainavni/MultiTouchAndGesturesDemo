a3cbae876225940357df04264cfc4b36c2c4a6de
Avni Jain <aj.jainavni1998@gmail.com>
2017-09-11 22:39:55 +0530

    Second

--- app/src/main/java/com/example/avnijain/multitouchandgesturesdemo/GestureActivity.java
+++ app/src/main/java/com/example/avnijain/multitouchandgesturesdemo/GestureActivity.java
@@ -1,34 +1,154 @@
 package com.example.avnijain.multitouchandgesturesdemo;
 
+import android.gesture.Gesture;
+import android.gesture.GestureLibraries;
+import android.gesture.GestureLibrary;
+import android.gesture.GestureOverlayView;
+import android.gesture.Prediction;
 import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
-import android.view.View;
 import android.widget.RelativeLayout;
+import java.util.ArrayList;
 
-public class GestureActivity extends AppCompatActivity {
+public class GestureActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
+
 
     RelativeLayout relativeLayout;
     GestureDetectorCompat gestureDetectorCompat;
-
+    String TAG = "GestureActivityTag";
+    GestureOverlayView gestureOverlayView;
+    GestureLibrary library;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gesture);
-        relativeLayout = (RelativeLayout)findViewById(R.id.relaiveLayout);
+        relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+        library = GestureLibraries.fromRawResource(this, R.raw.gestures);
+        library.load();
+        gestureOverlayView = (GestureOverlayView) findViewById(R.id.gestureOverlay);
 
-        relativeLayout.setOnTouchListener(new View.OnTouchListener() {
+        gestureOverlayView.addOnGesturePerformedListener(new GestureOverlayView.OnGesturePerformedListener() {
             @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                return false;
+            public void onGesturePerformed(GestureOverlayView overlay, Gesture gesture) {
+                Log.i(TAG, "onGesturePerformed");
+
+                ArrayList<Prediction> predictions = library.recognize(gesture);
+                Log.i(TAG, "Predictions size " + predictions.size());
+
+                for(Prediction prediction : predictions){
+                    Log.i(TAG, prediction.name +" " +prediction.score+"");
+                }
             }
         });
 
-      //  gestureDetectorCompat = new GestureDetectorCompat(this,this);
+//        gestureDetectorCompat = new GestureDetectorCompat(this, this);
+//        relativeLayout.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                gestureDetectorCompat.onTouchEvent(event);
+//                return true;
+//            }
+//        });
+    }
 
+    /**
+     * Notified when a tap occurs with the down {@link MotionEvent}
+     * that triggered it. This will be triggered immediately for
+     * every down event. All other events should be preceded by this.
+     *
+     * @param e The down motion event.
+     */
+    @Override
+    public boolean onDown(MotionEvent e) {
+        Log.i(TAG, "onDown");
+        return true;
+    }
 
+    /**
+     * The user has performed a down {@link MotionEvent} and not performed
+     * a move or up yet. This event is commonly used to provide visual
+     * feedback to the user to let them know that their action has been
+     * recognized i.e. highlight an element.
+     *
+     * @param e The down motion event
+     */
+    @Override
+    public void onShowPress(MotionEvent e) {
+        Log.i(TAG, "onShowPress");
+    }
+
+    /**
+     * Notified when a tap occurs with the up {@link MotionEvent}
+     * that triggered it.
+     *
+     * @param e The up motion event that completed the first tap
+     * @return true if the event is consumed, else false
+     */
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        Log.i(TAG, "onSingleTapUp");
+        return true;
+    }
+
+    /**
+     * Notified when a scroll occurs with the initial on down {@link MotionEvent} and the
+     * current move {@link MotionEvent}. The distance in x and y is also supplied for
+     * convenience.
+     *
+     * @param e1        The first down motion event that started the scrolling.
+     * @param e2        The move motion event that triggered the current onScroll.
+     * @param distanceX The distance along the X axis that has been scrolled since the last
+     *                  call to onScroll. This is NOT the distance between {@code e1}
+     *                  and {@code e2}.
+     * @param distanceY The distance along the Y axis that has been scrolled since the last
+     *                  call to onScroll. This is NOT the distance between {@code e1}
+     *                  and {@code e2}.
+     * @return true if the event is consumed, else false
+     */
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        Log.i(TAG, "onScroll");
+
+        return true;
+    }
+
+    /**
+     * Notified when a long press occurs with the initial on down {@link MotionEvent}
+     * that trigged it.
+     *
+     * @param e The initial on down motion event that started the longpress.
+     */
+    @Override
+    public void onLongPress(MotionEvent e) {
+        Log.i(TAG, "onLongPress");
 
     }
+
+    /**
+     * Notified of a fling event when it occurs with the initial on down {@link MotionEvent}
+     * and the matching up {@link MotionEvent}. The calculated velocity is supplied along
+     * the x and y axis in pixels per second.
+     *
+     * @param e1        The first down motion event that started the fling.
+     * @param e2        The move motion event that triggered the current onFling.
+     * @param velocityX The velocity of this fling measured in pixels per second
+     *                  along the x axis.
+     * @param velocityY The velocity of this fling measured in pixels per second
+     *                  along the y axis.
+     * @return true if the event is consumed, else false
+     */
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        Log.i(TAG, "onFling");
+
+        return true;
+    }
 }
+
